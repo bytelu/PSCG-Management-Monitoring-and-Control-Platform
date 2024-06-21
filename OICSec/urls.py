139cace8b355f804b44d9305c8fb04d9c5df1709
bytelu <luan78hihe@gmail.com>
2024-06-21 12:42:54 -0600

    Pagina con auditorias y filtro Scraping para extraer datos con guardado en DB Detalle de auditorias con formulario editable

---
+++ OICSec/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+
+from .views import login_view, home_view, auditorias_view, minutas_view, perfil_view, logout_view, upload_paa_view, \
+    auditoria_detalle_view
+
+urlpatterns = [
+    path("", login_view, name="login"),
+    path("home/", home_view, name="home"),
+    path("perfil/", perfil_view, name="perfil"),
+    path("logout/", logout_view, name="logout"),
+    path("auditorias/", auditorias_view, name="auditorias"),
+    path("auditorias/PAA/", upload_paa_view, name="uploadPaa"),
+    path("auditorias/<int:auditoria_id>/", auditoria_detalle_view, name="auditoria_detalle"),
+    path("minutas/", minutas_view, name="minutas"),
+]
