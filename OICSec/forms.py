a88366d26ac9fd3a27efb72e201930ed5a359316
bytelu <luan78hihe@gmail.com>
2024-07-10 13:50:25 -0600

    Refactorización de formularios en forms.py

--- OICSec/forms.py
+++ OICSec/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Auditoria, ActividadFiscalizacion, Oic, ControlInterno, Intervencion
 
 
-class AuditoriaForm(forms.ModelForm):
+class BaseForm(forms.ModelForm):
     anyo = forms.IntegerField(
         label='Año de actividad de fiscalización',
         required=False,
@@ -20,19 +20,33 @@ class AuditoriaForm(forms.ModelForm):
         widget=forms.Select(attrs={'class': 'form-control select2'})
     )
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance.id_actividad_fiscalizacion:
+            self.fields['anyo'].initial = self.instance.id_actividad_fiscalizacion.anyo
+            self.fields['trimestre'].initial = self.instance.id_actividad_fiscalizacion.trimestre
+            self.fields['id_oic'].initial = self.instance.id_actividad_fiscalizacion.id_oic
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        if instance.id_actividad_fiscalizacion is None:
+            instance.id_actividad_fiscalizacion = ActividadFiscalizacion()
+        instance.id_actividad_fiscalizacion.anyo = self.cleaned_data.get('anyo')
+        instance.id_actividad_fiscalizacion.trimestre = self.cleaned_data.get('trimestre')
+        instance.id_actividad_fiscalizacion.id_oic = self.cleaned_data.get('id_oic')
+        if commit:
+            instance.id_actividad_fiscalizacion.save()
+            instance.save()
+        return instance
+
+
+class AuditoriaForm(BaseForm):
     class Meta:
         model = Auditoria
-        fields = ['denominacion',
-                  'numero',
-                  'objetivo',
-                  'oportunidad',
-                  'alcance',
-                  'ejercicio',
-                  'unidad',
-                  'id_materia',
-                  'id_enfoque',
-                  'id_programacion',
-                  'id_temporalidad']
+        fields = [
+            'denominacion', 'numero', 'objetivo', 'oportunidad', 'alcance', 'ejercicio',
+            'unidad', 'id_materia', 'id_enfoque', 'id_programacion', 'id_temporalidad'
+        ]
         labels = {
             'denominacion': 'Denominación:',
             'numero': 'Número:',
@@ -60,44 +74,14 @@ class AuditoriaForm(forms.ModelForm):
             'id_temporalidad': forms.Select(attrs={'class': 'form-control select2'})
         }
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        if self.instance.id_actividad_fiscalizacion:
-            self.fields['anyo'].initial = self.instance.id_actividad_fiscalizacion.anyo
-            self.fields['trimestre'].initial = self.instance.id_actividad_fiscalizacion.trimestre
-            self.fields['id_oic'].initial = self.instance.id_actividad_fiscalizacion.id_oic
 
-    def save(self, commit=True):
-        instance = super().save(commit=False)
-        if instance.id_actividad_fiscalizacion is None:
-            instance.id_actividad_fiscalizacion = ActividadFiscalizacion()
-        instance.id_actividad_fiscalizacion.anyo = self.cleaned_data.get('anyo')
-        instance.id_actividad_fiscalizacion.trimestre = self.cleaned_data.get('trimestre')
-        instance.id_actividad_fiscalizacion.id_oic = self.cleaned_data.get('id_oic')
-        if commit:
-            instance.id_actividad_fiscalizacion.save()
-            instance.save()
-        return instance
-
-
-class ControlForm(forms.ModelForm):
-    anyo = forms.IntegerField(label='Año de actividad de fiscalización', required=False,
-                              widget=forms.NumberInput(attrs={'class': 'form-control'}))
-    trimestre = forms.IntegerField(label='Trimestre de actividad de fiscalización', required=False,
-                                   widget=forms.NumberInput(attrs={'class': 'form-control'}))
-    id_oic = forms.ModelChoiceField(queryset=Oic.objects.all(), label='OIC', required=False,
-                                    widget=forms.Select(attrs={'class': 'form-control select2'}))
-
+class ControlForm(BaseForm):
     class Meta:
         model = ControlInterno
-        fields = ['numero',
-                  'area',
-                  'ejercicio',
-                  'denominacion',
-                  'objetivo',
-                  'id_tipo_revision',
-                  'id_programa_revision',
-                  ]
+        fields = [
+            'numero', 'area', 'ejercicio', 'denominacion', 'objetivo', 'id_tipo_revision',
+            'id_programa_revision'
+        ]
         labels = {
             'numero': 'Número:',
             'area': 'Area:',
@@ -105,10 +89,8 @@ class ControlForm(forms.ModelForm):
             'denominacion': 'Denominación:',
             'objetivo': 'Objetivo:',
             'id_tipo_revision': 'Tipo de revision:',
-            'id_programa_revision': 'Programa de revision:',
-
+            'id_programa_revision': 'Programa de revision:'
         }
-
         widgets = {
             'numero': forms.NumberInput(attrs={'class': 'form-control'}),
             'area': forms.TextInput(attrs={'class': 'form-control'}),
@@ -116,64 +98,18 @@ class ControlForm(forms.ModelForm):
             'denominacion': forms.Textarea(attrs={'class': 'form-control', 'rows': 2}),
             'objetivo': forms.Textarea(attrs={'class': 'form-control', 'rows': 2}),
             'id_tipo_revision': forms.Select(attrs={'class': 'form-control select2'}),
-            'id_programa_revision': forms.Select(attrs={'class': 'form-control select2'}),
-
+            'id_programa_revision': forms.Select(attrs={'class': 'form-control select2'})
         }
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        if self.instance.id_actividad_fiscalizacion:
-            self.fields['anyo'].initial = self.instance.id_actividad_fiscalizacion.anyo
-            self.fields['trimestre'].initial = self.instance.id_actividad_fiscalizacion.trimestre
-            self.fields['id_oic'].initial = self.instance.id_actividad_fiscalizacion.id_oic
 
-    def save(self, commit=True):
-        instance = super().save(commit=False)
-        if instance.id_actividad_fiscalizacion is None:
-            instance.id_actividad_fiscalizacion = ActividadFiscalizacion()
-        instance.id_actividad_fiscalizacion.anyo = self.cleaned_data.get('anyo')
-        instance.id_actividad_fiscalizacion.trimestre = self.cleaned_data.get('trimestre')
-        instance.id_actividad_fiscalizacion.id_oic = self.cleaned_data.get('id_oic')
-        if commit:
-            instance.id_actividad_fiscalizacion.save()
-            instance.save()
-        return instance
-
-
-class IntervencionForm(forms.ModelForm):
-    anyo = forms.IntegerField(
-        label='Año de actividad de fiscalización',
-        required=False,
-        widget=forms.NumberInput(attrs={'class': 'form-control'})
-    )
-    trimestre = forms.IntegerField(
-        label='Trimestre de actividad de fiscalización',
-        required=False,
-        widget=forms.NumberInput(attrs={'class': 'form-control'})
-    )
-    id_oic = forms.ModelChoiceField(
-        queryset=Oic.objects.all(),
-        label='OIC',
-        required=False,
-        widget=forms.Select(attrs={'class': 'form-control select2'})
-    )
-
+class IntervencionForm(BaseForm):
     class Meta:
         model = Intervencion
-        fields = ['numero',
-                  'unidad',
-                  'denominacion',
-                  'ejercicio',
-                  'alcance',
-                  'antecedentes',
-                  'fuerza_auditores',
-                  'fuerza_responsables',
-                  'fuerza_supervision',
-                  'inicio',
-                  'termino',
-                  'objetivo',
-                  'id_tipo_intervencion'
-                  ]
+        fields = [
+            'numero', 'unidad', 'denominacion', 'ejercicio', 'alcance', 'antecedentes',
+            'fuerza_auditores', 'fuerza_responsables', 'fuerza_supervision', 'inicio',
+            'termino', 'objetivo', 'id_tipo_intervencion'
+        ]
         labels = {
             'numero': 'Número:',
             'unidad': 'Unidad:',
@@ -189,7 +125,6 @@ class IntervencionForm(forms.ModelForm):
             'objetivo': 'Objetivo:',
             'id_tipo_intervencion': 'Tipo de Intervencion:'
         }
-
         widgets = {
             'numero': forms.NumberInput(attrs={'class': 'form-control'}),
             'unidad': forms.TextInput(attrs={'class': 'form-control'}),
@@ -203,24 +138,5 @@ class IntervencionForm(forms.ModelForm):
             'inicio': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
             'termino': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
             'objetivo': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
-            'id_tipo_intervencion': forms.Select(attrs={'class': 'form-control select2'}),
+            'id_tipo_intervencion': forms.Select(attrs={'class': 'form-control select2'})
         }
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        if self.instance.id_actividad_fiscalizacion:
-            self.fields['anyo'].initial = self.instance.id_actividad_fiscalizacion.anyo
-            self.fields['trimestre'].initial = self.instance.id_actividad_fiscalizacion.trimestre
-            self.fields['id_oic'].initial = self.instance.id_actividad_fiscalizacion.id_oic
-
-    def save(self, commit=True):
-        instance = super().save(commit=False)
-        if instance.id_actividad_fiscalizacion is None:
-            instance.id_actividad_fiscalizacion = ActividadFiscalizacion()
-        instance.id_actividad_fiscalizacion.anyo = self.cleaned_data.get('anyo')
-        instance.id_actividad_fiscalizacion.trimestre = self.cleaned_data.get('trimestre')
-        instance.id_actividad_fiscalizacion.id_oic = self.cleaned_data.get('id_oic')
-        if commit:
-            instance.id_actividad_fiscalizacion.save()
-            instance.save()
-        return instance
